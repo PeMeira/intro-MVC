a155e9f8f3e7079ec21140040b6501653460adb5
PeMeira <phsmeira00@gmail.com>
2025-03-31 15:14:35 -0300

    Usando LocalStorage

--- js/app.js
+++ js/app.js
@@ -1,40 +1,4 @@
-//objeto Alunos
-const alunos = [
-    {
-      nome: "chico melato",
-      notas: {
-        backend_1: [7, 8.5, 9, 6.5],
-        frontend_2: [2, 2, 2, 2],
-        bancodados: [2, 2, 3, 3],
-        ferramentas: [3, 3, 3, 3],
-      },
-    },
-    {
-      nome: "talita lima",
-      notas: {
-        backend_1: [4, 4, 4, 4],
-        frontend_2: [4, 4, 5, 5],
-        bancodados: [5, 5, 6, 6],
-        ferramentas: [7, 7, 8, 9],
-      },
-    },
-    {
-      nome: "fulano da silva",
-      notas: {
-        backend_1: [3, 3, 3, 3],
-        frontend_2: [4, 4, 7, 7],
-        bancodados: [5, 6, 8, 6],
-        ferramentas: [7, 7, 8, 9],
-      },
-    },
-  ];   
-
-
-  const alunoService = new AlunoService();
-
-alunos.forEach(aluno => {
-  alunoService.add(new AlunoModel(aluno)) 
-})
+const alunoService = new AlunoService();
 
 const alunoView = new AlunoView(document.querySelector('[data-table-alunos]'))
 const alunoController = new AlunoController(alunoService, alunoView)
--- js/services/aluno.services.js
+++ js/services/aluno.services.js
@@ -1,13 +1,16 @@
 class AlunoService {
     constructor() {
         this.alunos = []
+        this.updateListAlunosFromLocalStorage()
     }
 
     add(aluno) {
         if(!aluno instanceof AlunoModel) {
             throw new Error('O aluno deve ser uma instância de AlunoModel')
         }
+
         this.alunos.push(aluno)
+        this.updateLocalStorage()
     }
 
     update(aluno) {
@@ -16,5 +19,21 @@ class AlunoService {
 
     searchById(id) {
         return this.alunos.find(aluno => aluno._id === id)
+    }
+
+    updateLocalStorage() {
+        let alunos = JSON.stringify(this.alunos)
+        localStorage.setItem('alunos', alunos)
+    }
+
+    updateListAlunosFromLocalStorage() {
+        let local = localStorage.getItem('alunos')
+        let alunos = []
+        if(local) {
+            alunos = JSON.parse(local)
+        }
+        alunos.forEach(aluno => {
+            this.add(new AlunoModel(aluno))
+        })
     }
 }
